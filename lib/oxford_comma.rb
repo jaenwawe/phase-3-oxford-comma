d96f7b6068f7d1ef3dc3c5119f767806edaa37a4
jaenwawe <nwawe.jae@gmail.com>
2021-09-21 18:52:37 -0500

    commit tree 3aa7ff8a4bc04e92675f5f17cbc7511e27e42b4a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     if array.size < 2  array
+#     if array.size == 2    array.join(" and ") 
+#     elsif array.size > 3 array.join(" and ")
+#     array[-1] = "and #{array[-1]}"
+#     end
 
-end
+#   array.join(", ")  
+    
+# end
+
+
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+  
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
+  end
